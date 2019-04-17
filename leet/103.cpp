25e2a18907c12b85066d13bd86e3be209ece7eeb
wenhuijia <wenhuijia@tencent.com>
2019-04-17 18:46:43 +0800

    add file

--- leet/103.cpp
+++ leet/103.cpp
@@ -10,10 +10,29 @@
 #include <unordered_map>
 #include <queue>
 #include <stack>
-#include <iterator>
-#include <algorithm>
+#include <benchmark/benchmark.h>
+#include <gtest/gtest.h>
 
 using namespace std;
+
+/*
+ * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *  / \
+ * 15  7
+ * 
+ * return its zigzag level order traversal as:
+ *  [
+ *       [3],
+ *       [20,9],
+ *       [15,7]
+ *  ]
+ */
 
 /**
  * Definition for a binary tree node.
@@ -33,147 +52,80 @@ class Solution {
                 return ans;
             }
 
-            list<TreeNode*> l;
-            l.push_front(root);
+            queue<TreeNode*> q;
+            q.push(root);
+            bool reverse = false;
 
-            vector<int> tmp;
-            list<TreeNode*> level;
-            int direction = -1;
-            while(!l.empty()) {
-                tmp.clear();
-                int size = l.size();
-                while (size --) {
-                    auto& i = l.front();
-                    tmp.push_back(i->val);
-                    if (i->left) {
-                        l.push_back(i->left);
-                    }
-                    if (i->right) {
-                        l.push_back(i->right);
-                    }
-                    l.pop_front();
-                }
-                if (-1 == direction) {
-                    direction = 1;
-                } else {
-                    std::reverse(tmp.begin(), tmp.end());
-                    direction = -1;
-                }
-                ans.push_back(tmp);
-            }
-
-            return ans;
-        }
-
-        vector<vector<int>> levelOrder(TreeNode* root) {
-            vector<vector<int>> ans;
-            if (nullptr == root) {
-                return ans;
-            }
-            queue<pair<TreeNode*, int>> q;
-            q.push(pair<TreeNode*, int>(root, 1));
-
-            int level = 1;
-
-            vector<int> tmp;
             while (!q.empty()) {
-                auto& x = q.front();
-                q.pop();
-                if (x.second == level) {
-                    tmp.push_back(x.first->val);
-                } else {
-                    level += 1;
-                    ans.push_back(tmp);
-                    tmp.clear();
-                    tmp.push_back(x.first->val);
+                int size = q.size(); 
+                vector<int> level;
+                for (int i=0; i<size; i++) {
+                    auto it = q.front();
+                    level.push_back(it->val);
+                    q.pop();
+                    if (it->left) q.push(it->left);
+                    if (it->right) q.push(it->right);
                 }
-                if (x.first->left) {
-                    q.push(pair<TreeNode*, int>(x.first->left, level+1));
+                if (reverse) {
+                    std::reverse(level.begin(), level.end());
                 }
-                if (x.first->right) {
-                    q.push(pair<TreeNode*, int>(x.first->right, level+1));
-                }
-            }
-            if (!tmp.empty()) {
-                ans.push_back(tmp);
-            }
-            for (int i=1; i<ans.size(); i+=2) {
-                std::reverse(ans[i].begin(), ans[i].end());
+                ans.push_back(level);
+                reverse = !reverse;
             }
 
             return ans;
         }
 };
 
-vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-    vector<vector<int>> result;
-    if (!root) return result;
-    list<TreeNode *> cache(1, root);
-    int one = 1, two = 0;
-    result.emplace_back();
-    while (!cache.empty()) {
-        if (!one) {
-            result.emplace_back();
-            one = two;
-            two = 0;
-        }
-        TreeNode *top = cache.front();
-        result.back().push_back(top->val);
-        cache.pop_front();
-        one--;
-        if (top->left) {
-            cache.push_back(top->left);
-            two++;
-        }
-        if (top->right) {
-            cache.push_back(top->right);
-            two++;
-        }
-    }
-    int len = result.size();
-    for (int i = 1; i < len; i += 2)
-        reverse(result[i].begin(), result[i].end());
-    return result;
+namespace best {
+    class Solution {
+        public:
+            vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
+                vector<vector<int>> result;
+                traverse(root, 1, result, true);
+                return result;
+            }
+
+            void traverse(TreeNode *root, size_t level, vector<vector<int>> &result,
+                    bool left_to_right) {
+                if (!root) return;
+                if (level > result.size())
+                    result.push_back(vector<int>());
+                if (left_to_right)
+                    result[level-1].push_back(root->val);
+                else
+                    result[level-1].insert(result[level-1].begin(), root->val);
+                traverse(root->left, level+1, result, !left_to_right);
+                traverse(root->right, level+1, result, !left_to_right);
+            }
+    };
 }
 
-
-
-#include <benchmark/benchmark.h>
-TreeNode* CreateTree() {
-    TreeNode *n15 = new TreeNode(15);
-    TreeNode *n2 = new TreeNode(2);
-    TreeNode *n20 = new TreeNode(20);
-    TreeNode *n1 = new TreeNode(1);
-    TreeNode *n3 = new TreeNode(3);
-    TreeNode *n17 = new TreeNode(17);
-    TreeNode *n22 = new TreeNode(22);
-
-    n15->left = n2;
-    n15->right = n20;
-
-    n2->left = n1;
-    n2->right = n3;
-
-    n20->left = n17;
-    n20->right = n22;
-
-    return n15;
-}
-TreeNode* root = CreateTree();
-
-static void BM_best(benchmark::State& state) {
-    for (auto _ : state)
-        zigzagLevelOrder(root);
-}
-
-static void BM_mine(benchmark::State& state) {
+TEST(TestMine, HandleNoraml) {
     Solution s;
-    for (auto _ : state) 
-        s.zigzagLevelOrder(root);
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node9 = new TreeNode(9);
+    TreeNode* node20 = new TreeNode(20);
+    TreeNode* node15 = new TreeNode(15);
+    TreeNode* node7 = new TreeNode(7);
 
+    node3->left = node9;
+    node3->right = node20;
+    node9->left = node15;
+    node9->right = node7;
+
+    std::vector<vector<int>> expect = {{3}, {20, 9}, {15, 7}};
+    std::vector<vector<int>> ans = s.zigzagLevelOrder(node3);
+    EXPECT_EQ(expect, ans);
 }
 
-// Register the function as a benchmark
-BENCHMARK(BM_mine); //用于注册测试函数
-BENCHMARK(BM_best); //用于注册测试函数
-BENCHMARK_MAIN(); //程序入口
+#ifdef _TEST_
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+#else
+//Register the function as abenchmark
+BENCHMARK_MAIN();   //程序入口
+#endif
+
--- leet/build.sh
+++ leet/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "compiling test version"
-clang++ -D_TEST_ -std=c++11 $1.cpp  -lbenchmark -lpthread -lgtest -o $1_test
+clang++ -g -D_TEST_ -std=c++11 $1.cpp  -lbenchmark -lpthread -lgtest -o $1_test
 echo "compiling benchmark version"
 g++  -std=c++11 $1.cpp  -lbenchmark -lpthread -lgtest -o $1_bm
