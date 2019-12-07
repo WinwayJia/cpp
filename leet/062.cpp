1510400a3575d3154b79566883badc307aa59893
winway <godlikejia@126.com>
2019-12-07 22:28:38 +0800

    add file

--- leet/062.cpp
+++ leet/062.cpp
@@ -17,33 +17,32 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
-        int ans = 0;
-        bool flag = true;
-        for (int i=s.size()-1; i>=0; i--) {
-            if (flag) {
-                if (' ' != s[i]) {
-                    ans += 1;
-                } else {
-                    break;
-                }
-            } else {
-                if (' ' != s[i]) {
-                    flag = true;
-                    ans += 1;
-                }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> vec(n, vector<int>(m, 0));
+        for (int i=1; i<m; i++) {
+            vec[0][i] = 1;
+        }
+        for (int i=1; i<n; i++) {
+            vec[i][0] = 1;
+        }
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                vec[j][i] = vec[j][i-1] + vec[j-1][i];
             }
         }
 
-        return ans;
+        return vec[n-1][m-1];
     }
 };
 
 TEST(TestSolution, HandlerNormal) {
     Solution s;
     {
-        string str = "a ";
-        EXPECT_EQ(1, s.lengthOfLastWord(str))
+        EXPECT_EQ(3, s.uniquePaths(3, 2));
+    }
+    {
+        EXPECT_EQ(28, s.uniquePaths(7, 3));
     }
 }
 
